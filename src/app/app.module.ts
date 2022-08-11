2f85b5e2117d2436673038d7bb88d644ee050b70
CesarVlez <106609483+CesarVlez@users.noreply.github.com>
2022-08-11 08:24:38 -0400

    project created, navbar, footer and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './sharepages/navbar/navbar.component';
+import { FooterComponent } from './sharepages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
